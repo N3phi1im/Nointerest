cfe99fd37045caf12e59d2c6ceffc70b15c9c85a
n3phi1im <n3ph2535@gmail.com>
2015-08-03 15:55:26 -0700

    Adding in Post Routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -28,7 +28,7 @@ router.post('/Register', function(req, res, next) {
 	// Passport Local authentication with JWT
 
 	router.post('/Login', function(req, res, next) {
-		if(!req.body.username || !req.body.password) return res.status(400).send('Please complete all fields');
+		if(!req.body.userName || !req.body.password) return res.status(400).send('Please complete all fields');
 		passport.authenticate('local', function(err, user, info) {
 			if(err)  return next(err);
 			if(user)  return res.json({token: user.generateJWT()});
