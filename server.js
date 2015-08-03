f06e2c97b026f16f74946845d882de778c469277
n3phi1im <n3ph2535@gmail.com>
2015-08-03 10:54:02 -0700

    Fixed missing dependencies

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
 var app = express();
 
 // Models for Passport
