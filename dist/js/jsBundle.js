2fd6dda4e6c9a2161bba61ffe41dd1282779f43b
Philip lanphar <pjlanphar@gmail.com>
2015-08-05 16:11:38 -0700

    updated add user

--- dist/js/jsBundle.js
+++ dist/js/jsBundle.js
@@ -170,6 +170,32 @@ angular.module('app').controller('ModalInstanceCtrl', ["$scope", "$modalInstance
   };
 }]);
 
+angular.module('app').filter('searchFilter', function() {
+   return function(items, word) {
+    if(!word) return items;
+    var filtered = [];
+
+    var pushed;
+    angular.forEach(items, function(item) {
+      if(item.hasOwnProperty('title'))
+        if(item.title.toLowerCase().indexOf(word.toLowerCase()) !== -1){
+            filtered.push(item);
+            return;
+        }
+      if(item.hasOwnProperty('hashtag'))
+        if(item.hashtag.join().toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+          filtered.push(item);
+          return;
+        }
+      if(item.username.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+        filtered.push(item);
+      }
+    });
+
+    return filtered;
+  };
+});
+
 (function() {
 	'use strict';
 	angular.module('app')
@@ -343,29 +369,3 @@ angular.module('app').controller('ModalInstanceCtrl', ["$scope", "$modalInstance
 		}
 	}
 })();
-
-angular.module('app').filter('searchFilter', function() {
-   return function(items, word) {
-    if(!word) return items;
-    var filtered = [];
-
-    var pushed;
-    angular.forEach(items, function(item) {
-      if(item.hasOwnProperty('title'))
-        if(item.title.toLowerCase().indexOf(word.toLowerCase()) !== -1){
-            filtered.push(item);
-            return;
-        }
-      if(item.hasOwnProperty('hashtag'))
-        if(item.hashtag.join().toLowerCase().indexOf(word.toLowerCase()) !== -1) {
-          filtered.push(item);
-          return;
-        }
-      if(item.username.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
-        filtered.push(item);
-      }
-    });
-
-    return filtered;
-  };
-});
